78dbf5466a693469ec711cdc4e4a2e06c7619cc9
Selam A. <selamsasnake@gmail.com>
2024-12-10 02:51:26 +0300

    update: removed cleaning steps

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -11,36 +11,11 @@ def explore_data(df):
     print("First 5 rows:", df.head(5))
     print("Data types of the columns:", df.dtypes)
 
-#change to appropriate datatypes
-def convert_datatypes(df):
-    df['Timestamp'] = pd.to_datetime(df['Timestamp'])
-    return df
-
-#check for missing values
-def handle_missing_values(df):
-    if (df.isnull().sum()) > 0:
-        print(df.isnull().sum() > 0)
-    return df
-
-
-def drop_columns(df):
-    df = df.drop(['Comments'], axis=1).copy()
-    return df
-
-def remove_negative_values(df):
-    df = df[df['GHI'] >= 0]
-    df = df[df['DNI'] >= 0]
-    df = df[df['DHI'] >= 0]
-    return df
 
 def main():
     file_path = 'data/benin-malanville.csv'
     df = load_data(file_path)
     explore_data(df)
-    convert_datatypes(df)
-    handle_missing_values(df)
-    drop_columns(df)
-    remove_negative_values(df)
 
 
 if __name__ == "__main__":

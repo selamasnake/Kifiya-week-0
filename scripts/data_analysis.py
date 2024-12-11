b661fca28d9996d21c16c9ccf794f62a485e24b7
Selam A. <selamsasnake@gmail.com>
2024-12-11 17:11:28 +0300

    feat: added resample data functions

--- scripts/data_analysis.py
+++ scripts/data_analysis.py
@@ -11,12 +11,25 @@ def convert_datatypes(df):
 def summary_statistics(df):
     return df.describe()
 
+def resample_data_h(df):
+    df.set_index('Timestamp', inplace=True)
+    hourly_data = df.resample('h').mean()
+    return hourly_data
+
+def resample_data_m(df):
+    df.set_index('Timestamp')
+    monthly_data = df.resample('h').mean()
+    return df
+
+
 
 def main():
     file_path = 'data/prepared/benin_prepared.csv'
     df = load_data(file_path)
     convert_datatypes(df)
     summary_statistics(df)
+    resample_data_h(df)
+    resample_data_m(df)
 
 if __name__ == "__main__":
     main()
